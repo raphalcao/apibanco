be4ed6fe26a205dca537abbad1caa63f347761dd
raphalcao <raphalcao@gmail.com>
2020-05-11 20:38:52 -0300

    Concluido os testes da API

--- tests/Feature/ContaTest.php
+++ tests/Feature/ContaTest.php
@@ -10,7 +10,7 @@ class ContaTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * A basic feature test example.
+     * Efetua um teste para exibir o saldo.
      *
      * @return void
      */
@@ -25,4 +25,77 @@ class ContaTest extends TestCase
                 'saldo' => 100
             ]);
     }
+
+    /**
+     * Efetua um teste para a função depositar.
+     *
+     * @return void
+     */
+    public function testDepositar()
+    {
+        $response = $this->get('/api/depositar/123456/100');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'conta' => 123456,
+                'saldo' => 100
+            ]);
+    }
+
+    /**
+     * Testa a funcionalidade de saque.
+     *
+     * @return void
+     */
+    public function testSacar()
+    {
+        $this->get('/api/depositar/1234567/450');
+        $response = $this->get('/api/sacar/1234567/230');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'conta' => 1234567,
+                'saldo' => 220,
+            ]);
+
+    }
+
+    /**
+     * Testa a validação de conta.
+     *
+     * @return void
+     */
+    public function testValidarConta()
+    {
+        
+        $response = $this->get('/api/saldo/12345');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([                
+                'erro' => 'Conta inexistente'
+            ]);
+    }
+
+    /**
+     * Testa se foi efetuado um depósito com valor vazio.
+     *
+     * @return void
+     */
+    public function testVerificaValorVazio()
+    {
+                  
+        $response = $this->get('/api/depositar/54321/0');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                
+                'erro' => 'Deposito vazio invalido.'
+            ]);
+    }
+
+
 }
