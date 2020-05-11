c6f03f5c0cdc759a1ca6edb7f27b001e483da7e2
raphalcao <raphalcao@gmail.com>
2020-05-10 22:06:32 -0300

    Primeiro Commit API Web

---
+++ app/Http/Controllers/ContaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContaController extends Controller
+{
+    public function depositar() 
+    {
+
+    }
+
+    public function sacar() 
+    {
+        
+    }
+}
